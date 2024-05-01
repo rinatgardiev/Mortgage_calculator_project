17a741261e7c4847b37c8f2633279f2b85ac4d77
rinatgardiev <rinatgardiev@gmail.com>
2024-05-01 18:19:59 +0500

    Add payment schedule function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 def calculate_mortgage(principal, years, rate):
     r = rate / 100 / 12
@@ -21,5 +23,24 @@ def payment_schedule(principal, years, rate):
         interest = remaining_balance * r
         principal_paid = monthly_payment - interest
         remaining_balance -= principal_paid
+        if remaining_balance < 0:
+            remaining_balance = 0
+        else:
+            remaining_balance = remaining_balance
         schedule.append((i, monthly_payment, interest, principal_paid, remaining_balance))
     return schedule
+
+def create_calendar(number_of_payments, starting_date=datetime.now()):
+    calendar = []
+    for i in range(number_of_payments):
+        calendar.append((starting_date + relativedelta(months=i+1)).strftime("%B, %Y"))
+    return calendar
+
+def print_payment_schedule(schedule):
+    print("Payment schedule:")
+    print("№\tPayment\t\tInterest\tPrincipal\tRemaining balance\tDate")
+    
+    for payment in schedule:
+        print("%d\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f" % payment)
+
+print_payment_schedule(payment_schedule(100000, 10, 5))
